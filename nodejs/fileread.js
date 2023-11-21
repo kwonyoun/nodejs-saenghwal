9c25bf98879700584f19d4564ed1e59d4028daa9
kwon <kyjyj6334@gmail.com>
2023-11-21 17:59:15 +0900

    Node.js의 파일 읽기 기능

--- nodejs/fileread.js
+++ nodejs/fileread.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile('nodejs/sample.txt', 'utf8', (err, data) => {
+fs.readFile('sample.txt', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return;
