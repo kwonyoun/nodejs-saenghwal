5864e16eeebe392551af719f1775cc6e66acb779
kwon <kyjyj6334@gmail.com>
2023-11-22 18:23:54 +0900

    비교연산자

---
+++ syntax/comparison.js
@@ -0,0 +1,8 @@
+//node syntax/comparison.js
+
+console.log(1==1) //true
+console.log(1==2) //false
+console.log(1>1) //true
+console.log(2>1) //false
+console.log(1===1) //true
+console.log(1===2) //false
