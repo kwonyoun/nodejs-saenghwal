aef6aed307ef1bcc5ddb6eb6a29874e519169be3
kwon <kyjyj6334@gmail.com>
2023-12-19 18:08:17 +0900

    배열과 반복문

---
+++ syntax/array-loop.js
@@ -0,0 +1,10 @@
+var number = [1,22,43,111,2343];
+
+var i =0;
+var total=0;
+while (i< number.length) {
+    total = total+number[i];
+    console.log(total);
+    i=i+1;
+}
+console.log('tt'+total);
